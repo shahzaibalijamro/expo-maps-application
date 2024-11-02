1de19ee02d0feec9cdb4e4c1914698e54e8c3c22
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-11-03 02:29:26 +0500

    commands added

--- app/index.tsx
+++ app/index.tsx
@@ -315,47 +315,292 @@
 // });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useEffect, useState } from 'react';
+// import { View, Text, TextInput, TouchableOpacity, StyleSheet, SafeAreaView, Image } from 'react-native';
+// import * as SplashScreen from 'expo-splash-screen';
+// import { FontAwesome } from '@expo/vector-icons';
+// import AntDesign from '@expo/vector-icons/AntDesign';
+// import {
+//   OpenSans_400Regular,
+//   OpenSans_600SemiBold,
+//   OpenSans_700Bold,
+//   useFonts,
+// } from '@expo-google-fonts/open-sans';
+// SplashScreen.preventAutoHideAsync();
+// export default function JoinScreen() {
+//   const [phoneNumber, setPhoneNumber] = useState('');
+//   const [fontsLoaded] = useFonts({
+//     OpenSans_400Regular,
+//     OpenSans_600SemiBold,
+//     OpenSans_700Bold,
+//   });
+
+//   useEffect(() => {
+//     if (fontsLoaded) {
+//       SplashScreen.hideAsync();
+//     }
+//   }, [fontsLoaded]);
+
+//   if (!fontsLoaded) {
+//     return null;
+//   }
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <Text style={styles.title}>Join us via phone number</Text>
+//       <Text style={styles.subtitle}>We’ll text a code to verify your phone</Text>
+
+//       <View style={styles.inputContainer}>
+//         <TouchableOpacity style={styles.countryPicker}>
+//           <Text style={styles.flag}>🇵🇰</Text>
+//           {/* <FontAwesome name="chevron-down" size={12} color="#fff" /> */}
+//           <AntDesign name="caretdown" size={12} color="#fff" />
+//         </TouchableOpacity>
+
+//         <TextInput
+//           style={styles.input}
+//           placeholder="Enter phone number"
+//           placeholderTextColor="#888"
+//           keyboardType="phone-pad"
+//           value={phoneNumber}
+//           onChangeText={setPhoneNumber}
+//         />
+//       </View>
+
+//       <TouchableOpacity style={styles.nextButton}>
+//         <Text style={styles.nextButtonText}>Next</Text>
+//       </TouchableOpacity>
+
+//       <Text style={styles.orText}>Or login with</Text>
+
+//       <TouchableOpacity style={styles.googleButton}>
+//         <Image
+//           source={{ uri: 'https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA' }}
+//           style={{ width: 20, height: 20, marginRight: 8 }}
+//         />
+//         <Text style={styles.googleButtonText}>Continue with Google</Text>
+//       </TouchableOpacity>
+
+
+//       <Text style={styles.footerText}>
+//         Joining our app means you agree with our <Text style={styles.linkText}>Terms of Use</Text> and <Text style={styles.linkText}>Privacy Policy</Text>
+//       </Text>
+//     </SafeAreaView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#1c1f24',
+//     paddingHorizontal: 20,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   title: {
+//     fontSize: 23,
+//     // fontWeight: 'bold',
+//     fontFamily: 'OpenSans_700Bold',
+//     color: '#ffffff',
+//     marginBottom: 8,
+//     textAlign: 'center',
+//   },
+//   subtitle: {
+//     fontSize: 16,
+//     color: '#70737c',
+//     marginBottom: 20,
+//     textAlign: 'center',
+//     fontFamily: 'OpenSans_400Regular'
+//   },
+//   inputContainer: {
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     backgroundColor: '#2a2d33',
+//     borderRadius: 8,
+//     marginBottom: 20,
+//     width: '100%',
+//     paddingHorizontal: 10,
+//   },
+//   countryPicker: {
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     paddingRight: 10,
+//     paddingLeft: 5,
+//   },
+//   flag: {
+//     fontSize: 20,
+//     marginRight: 5,
+//   },
+//   input: {
+//     flex: 1,
+//     color: '#ffffff',
+//     paddingVertical: 10,
+//     fontSize: 16,
+//   },
+//   nextButton: {
+//     backgroundColor: '#77dd76',
+//     borderRadius: 8,
+//     paddingVertical: 12,
+//     alignItems: 'center',
+//     width: '100%',
+//     marginBottom: 5,
+//   },
+//   nextButtonText: {
+//     color: '#1c1f24',
+//     fontSize: 16,
+//     fontWeight: '600',
+//   },
+//   orText: {
+//     color: '#70737c',
+//     fontSize: 14,
+//     marginVertical: 15,
+//   },
+//   googleButton: {
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     backgroundColor: '#2a2d33',
+//     borderRadius: 8,
+//     paddingVertical: 12,
+//     width: '100%',
+//     justifyContent: 'center',
+//     marginBottom: 20,
+//   },
+//   googleButtonText: {
+//     color: '#ffffff',
+//     fontSize: 16,
+//   },
+//   footerText: {
+//     fontSize: 14,
+//     color: '#70737c',
+//     textAlign: 'center',
+//   },
+//   linkText: {
+//     color: '#77dd76',
+//     textDecorationLine: 'underline',
+//   },
+// });
+
+
+
+
 import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { AntDesign, FontAwesome } from '@expo/vector-icons';
 import * as SplashScreen from 'expo-splash-screen';
-import { FontAwesome } from '@expo/vector-icons'; // for Google icon
 import {
   OpenSans_400Regular,
   OpenSans_600SemiBold,
   OpenSans_700Bold,
   useFonts,
 } from '@expo-google-fonts/open-sans';
+
 SplashScreen.preventAutoHideAsync();
-export default function JoinScreen() {
+export default function ConfirmInfoScreen() {
   const [phoneNumber, setPhoneNumber] = useState('');
   const [fontsLoaded] = useFonts({
-        OpenSans_400Regular,
-        OpenSans_600SemiBold,
-        OpenSans_700Bold,
-      });
-    
-      useEffect(() => {
-        if (fontsLoaded) {
-          SplashScreen.hideAsync();
-        }
-      }, [fontsLoaded]);
-    
-      if (!fontsLoaded) {
-        return null;
-      }
+    OpenSans_400Regular,
+    OpenSans_600SemiBold,
+    OpenSans_700Bold,
+  });
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Join us via phone number</Text>
-      <Text style={styles.subtitle}>We’ll text a code to verify your phone</Text>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.backButton}>
+        <FontAwesome name="arrow-left" size={24} color="#fff" />
+      </TouchableOpacity>
+
+      <Text style={styles.header}>Confirm your information</Text>
+
+      <View style={styles.profileContainer}>
+        <Image
+          source={{ uri: 'https://instagram.fkhi22-1.fna.fbcdn.net/v/t51.2885-19/458925334_1022503086235063_7228415357725335161_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fkhi22-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=Ps5jUMZ6JS0Q7kNvgEpF_Bg&_nc_gid=ee7004b0d40e4e999bf80cf042311c5c&edm=ALGbJPMBAAAA&ccb=7-5&oh=00_AYDMCkcE2bFA-U7vET_cR-Kb46uQP99zxOgK2_pVv89EAg&oe=672C6994&_nc_sid=7d3ac5' }} // Replace with the user's profile image URL
+          style={styles.profileImage}
+        />
+        <TouchableOpacity style={styles.addIcon}>
+          <FontAwesome name="plus" size={16} color="#fff" />
+        </TouchableOpacity>
+      </View>
 
       <View style={styles.inputContainer}>
+        <Text style={styles.label}>Name</Text>
+        <TextInput style={styles.input} value="Shahzaib Ali" editable={false} />
+      </View>
+
+      <View style={{ ...styles.disabledInputContainer, borderRadius: 10 }}>
+        <Text style={{ color: '#8e8e93' }}>Email</Text>
+        <TextInput
+          style={{ color: '#8e8e93' }}
+          value="jamroshahzaibali69@gmail.com"
+          editable={false}
+        />
+      </View>
+
+      <View style={{ ...styles.inputContainer, flexDirection: 'row', alignItems: 'center', }}>
         <TouchableOpacity style={styles.countryPicker}>
-          <Text style={styles.flag}>🇺🇸</Text>
-          <FontAwesome name="chevron-down" size={12} color="#fff" />
+          <Text style={styles.flag}>🇵🇰</Text>
+          <AntDesign name="caretdown" size={10} color="#fff" />
         </TouchableOpacity>
 
         <TextInput
-          style={styles.input}
+          style={{
+            ...styles.input,
+            flex: 1,
+            color: '#ffffff',
+            paddingVertical: 5,
+            fontSize: 14,
+          }}
           placeholder="Enter phone number"
           placeholderTextColor="#888"
           keyboardType="phone-pad"
@@ -367,18 +612,7 @@ export default function JoinScreen() {
       <TouchableOpacity style={styles.nextButton}>
         <Text style={styles.nextButtonText}>Next</Text>
       </TouchableOpacity>
-
-      <Text style={styles.orText}>Or login with</Text>
-
-      <TouchableOpacity style={styles.googleButton}>
-        <FontAwesome name="google" size={20} color="#fff" style={{ marginRight: 8 }} />
-        <Text style={styles.googleButtonText}>Continue with Google</Text>
-      </TouchableOpacity>
-
-      <Text style={styles.footerText}>
-        Joining our app means you agree with our <Text style={styles.linkText}>Terms of Use</Text> and <Text style={styles.linkText}>Privacy Policy</Text>
-      </Text>
-    </SafeAreaView>
+    </View>
   );
 }
 
@@ -386,88 +620,101 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#1c1f24',
-    paddingHorizontal: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 20,
+    paddingTop: 80,
   },
-  title: {
-    fontSize: 23,
+  backButton: {
+    alignSelf: 'flex-start',
+    marginBottom: 20,
+  },
+  header: {
+    fontSize: 24,
     // fontWeight: 'bold',
     fontFamily: 'OpenSans_700Bold',
-    color: '#ffffff',
-    marginBottom: 8,
-    textAlign: 'center',
-  },
-  subtitle: {
-    fontSize: 16,
-    color: '#70737c',
+    color: '#fff',
     marginBottom: 20,
-    textAlign: 'center',
-    fontFamily: 'OpenSans_400Regular'
+    textAlign: 'left',
   },
-  inputContainer: {
-    flexDirection: 'row',
+  profileContainer: {
     alignItems: 'center',
-    backgroundColor: '#2a2d33',
-    borderRadius: 8,
-    marginBottom: 20,
-    width: '100%',
-    paddingHorizontal: 10,
+    marginBottom: 30,
+    width: 120,
+    marginHorizontal: 'auto'
+  },
+  profileImage: {
+    width: 140,
+    height: 140,
+    borderRadius: 100,
   },
   countryPicker: {
     flexDirection: 'row',
     alignItems: 'center',
     paddingRight: 10,
+    paddingLeft: 5,
+  },
+  addIcon: {
+    position: 'absolute',
+    bottom: 5,
+    right: 0,
+    backgroundColor: '#39d353',
+    width: 28,
+    height: 28,
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputContainer: {
+    width: '100%',
+    marginBottom: 20,
+    paddingHorizontal: 12,
+    paddingVertical: 7,
+    // flexDirection: 'row',
+    // alignItems: 'center',
+    backgroundColor: '#2a2d33',
+    borderRadius: 8,
+  },
+  label: {
+    color: '#8e8e93',
+  },
+  input: {
+    color: '#fff',
+  },
+  disabledInputContainer: {
+    backgroundColor: '#495563',
+    paddingHorizontal: 12,
+    paddingVertical: 7,
+    borderRadius: 10,
+    width: '100%',
+    marginBottom: 20,
+  },
+  countryContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#2c2c2e',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginRight: 10,
   },
   flag: {
     fontSize: 20,
     marginRight: 5,
   },
-  input: {
-    flex: 1,
-    color: '#ffffff',
-    paddingVertical: 10,
-    fontSize: 16,
+  countryCode: {
+    color: '#fff',
+    marginLeft: 5,
   },
   nextButton: {
-    backgroundColor: '#77dd76',
-    borderRadius: 8,
-    paddingVertical: 12,
+    backgroundColor: '#9ed90d',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 5,
     alignItems: 'center',
     width: '100%',
-    marginBottom: 5,
   },
   nextButtonText: {
-    color: '#1c1f24',
-    fontSize: 16,
-    fontWeight: '600',
-  },
-  orText: {
-    color: '#70737c',
-    fontSize: 14,
-    marginVertical: 15,
-  },
-  googleButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#2a2d33',
-    borderRadius: 8,
-    paddingVertical: 12,
-    width: '100%',
-    justifyContent: 'center',
-    marginBottom: 20,
-  },
-  googleButtonText: {
-    color: '#ffffff',
-    fontSize: 16,
-  },
-  footerText: {
-    fontSize: 14,
-    color: '#70737c',
-    textAlign: 'center',
-  },
-  linkText: {
-    color: '#77dd76',
-    textDecorationLine: 'underline',
+    color: 'black',
+    fontSize: 18,
+    fontWeight: 'semibold',
   },
 });
