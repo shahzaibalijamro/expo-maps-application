31ed55eec5e212fea0daca59fa0ff1d37401dd64
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-11-05 12:26:07 +0500

    commands added

--- app/index.tsx
+++ app/index.tsx
@@ -986,7 +986,8 @@ import {
   OpenSans_600SemiBold,
   OpenSans_700Bold,
 } from '@expo-google-fonts/open-sans';
-import { NotoSansEthiopic_100Thin,
+import {
+  NotoSansEthiopic_100Thin,
   NotoSansEthiopic_200ExtraLight,
   NotoSansEthiopic_300Light,
   NotoSansEthiopic_400Regular,
@@ -994,7 +995,8 @@ import { NotoSansEthiopic_100Thin,
   NotoSansEthiopic_600SemiBold,
   NotoSansEthiopic_700Bold,
   NotoSansEthiopic_800ExtraBold,
-  NotoSansEthiopic_900Black, } from '@expo-google-fonts/noto-sans-ethiopic';
+  NotoSansEthiopic_900Black,
+} from '@expo-google-fonts/noto-sans-ethiopic';
 import * as SplashScreen from 'expo-splash-screen';
 SplashScreen.preventAutoHideAsync();
 const App = () => {
@@ -1020,33 +1022,54 @@ const App = () => {
 
   return (
     <View style={styles.container}>
-      <View style={{backgroundColor: '#ffffff',width: 40,height:40,borderRadius: 150,justifyContent:'center',alignItems:'center',marginLeft: 'auto',marginTop:20,marginBottom:10}}>
+      <View style={{ backgroundColor: '#ffffff', width: 40, height: 40, borderRadius: 150, justifyContent: 'center', alignItems: 'center', marginLeft: 'auto', marginTop: 20, marginBottom: 10 }}>
         <Entypo name="cross" size={28} color="black" />
       </View>
       <View style={styles.header}>
-        <View style={{...styles.progressContainer,marginBottom:20}}>
-          <View style={[styles.progressBar, currentPage === 2 && styles.fullProgress]} />
+        <View style={{ ...styles.progressContainer, marginBottom: 20 }}>
+          <View style={{ ...styles.progressBar, backgroundColor: '#ffffff' }} />
+          <View style={[styles.progressBar, currentPage === 2 && { backgroundColor: '#ffffff' }]} />
         </View>
-        <Text style={styles.headerText}>What would you like to do?</Text>
+        <Text style={styles.headerText}>{currentPage === 2 ? 'Which ride do you need?' : 'What would you like to do?'}</Text>
       </View>
       {currentPage === 1 ? (
         <View style={styles.optionsContainer}>
           <TouchableOpacity style={styles.optionButton} onPress={nextPage}>
-            <Image style={styles.iconImg} source={require('../assets/images/car.png')}/>
-            <Text style={styles.optionText}>Get ride</Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Image style={styles.iconImg} source={require('../assets/images/car.png')} />
+              <Text style={styles.optionText}>Get ride</Text>
+            </View>
+            <Entypo name="chevron-thin-right" size={20} color="black" />
           </TouchableOpacity>
           <TouchableOpacity style={styles.optionButton} onPress={nextPage}>
-          <Image style={styles.iconImg} source={require('../assets/images/delivery-box.png')}/>
-          <Text style={styles.optionText}>Order delievery
-          </Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Image style={{ ...styles.iconImg, marginStart: 3, marginEnd: 17 }} source={require('../assets/images/delivery-box.png')} />
+              <Text style={styles.optionText}>Order delievery</Text>
+            </View>
+            <Entypo name="chevron-thin-right" size={20} color="black" />
           </TouchableOpacity>
         </View>
       ) : (
-        <View style={styles.questionContainer}>
-          <Text style={styles.questionText}>Where would you like the delivery?</Text>
-          {/* Additional questions or inputs can go here */}
+        <View style={styles.optionsContainer}>
+          <TouchableOpacity style={styles.optionButton} onPress={nextPage}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Image style={styles.iconImg} source={require('../assets/images/car.png')} />
+              <Text style={styles.optionText}>In the city</Text>
+            </View>
+            <Entypo name="chevron-thin-right" size={20} color="black" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.optionButton} onPress={nextPage}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Image style={styles.iconImg} source={require('../assets/images/car (1).png')} />
+              <Text style={styles.optionText}>To another city</Text>
+            </View>
+            <Entypo name="chevron-thin-right" size={20} color="black" />
+          </TouchableOpacity>
         </View>
       )}
+      {currentPage === 2 && <TouchableOpacity onPress={() => setCurrentPage(1)} style={{ backgroundColor: '#ffffff',position: 'absolute',bottom: 20,left:20, width: 50, height: 50, borderRadius: 10, justifyContent: 'center', alignItems: 'center',}}>
+        <Entypo name="chevron-thin-left" size={20} color="black" />
+      </TouchableOpacity>}
     </View>
   );
 };
@@ -1057,6 +1080,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#9ed90d', // Background color matching your design
     paddingHorizontal: 20,
     paddingTop: 30,
+    position: 'relative'
   },
   header: {
     marginBottom: 10,
@@ -1070,18 +1094,20 @@ const styles = StyleSheet.create({
   headerText: {
     fontSize: 24,
     fontFamily: 'OpenSans_700Bold',
-    marginVertical : 15
+    marginVertical: 15
   },
   progressContainer: {
     height: 5,
-    backgroundColor: '#3a3a3a',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
     marginTop: 10,
     borderRadius: 2,
   },
   progressBar: {
     height: '100%',
-    width: '50%', // Half-filled indicator
-    backgroundColor: '#ffffff',
+    width: '49.25%', // Half-filled indicator
+    backgroundColor: '#3a3a3a',
     borderRadius: 2,
   },
   fullProgress: {
@@ -1093,7 +1119,7 @@ const styles = StyleSheet.create({
   optionButton: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    justifyContent: 'space-between',
     backgroundColor: '#ffffff',
     padding: 15,
     borderRadius: 10,
